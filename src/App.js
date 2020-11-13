dd7e9f69a814e675cc1a127bff47dc977429518b
haileyli <hailey@Haileys-MacBook-Air-2.local>
2020-11-13 10:37:30 +0800

    get data from server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Loading from './Loading.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+
+    }
+  }
+
+  componentDidMount() {
+    this.getData()
+      .then(data => {
+        this.setState({
+          data: data,
+          isLoading: false
+        })
+      })
+  }
+
+  getData = () => {
+    return axios.get("http://5c92dbfae7b1a00014078e61.mockapi.io/owners")
+      .then((res) => {
+        return res.data
+      })
+      .catch(() => {
+        console.log("Error")
+      })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isLoading ?
+          <Loading /> :
+          <div>Data</div>
+        }
+      </React.Fragment>
+
+    );
+  }
+};
 
 export default App;
---
+++ src/Loading.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react';
+
+class Loading extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>loading</div>
+        );
+    }
+};
+
+export default Loading;
